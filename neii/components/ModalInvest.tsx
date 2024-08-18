e56c6270fc469dcceb1f041d4aaacceb15df1b25
Pedro-Ferreira-89 <81267194+Pedro-Ferreira-89@users.noreply.github.com>
2024-08-18 07:01:34 +0100

    Update types issues

--- neii/components/ModalInvest.tsx
+++ neii/components/ModalInvest.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import BigNumber from "bignumber.js";
+import axios from "axios";
 import Image from "next/image";
+import NaboxWindow from "../types/NaboxWindow";
 //import styles from '../../styles/Creator/MyPage/MyPage.module.css'
 //import { ConnectButton } from '@rainbow-me/rainbowkit';
 //import Link from "next/link";
@@ -20,7 +23,67 @@ import Image from "next/image";
 
 
 */
-export const ModalInvest = ({display, displayToggle, project}) => {
+
+interface Props{
+    display:boolean,
+    displayToggle:any
+    project:any
+}
+export const ModalInvest: React.FC<Props>  = ({display, displayToggle, project}) => {
+
+    const [balanceNuls, setBalanceNuls] = useState("")
+    const [valueIn, setValueIn] = useState("0")
+
+    const ip1 = "https://api.nuls.io/";
+
+
+
+    useEffect(() =>{
+
+        async function getTokenBalance() {
+
+            const naboxInfo:any = await (window as unknown as NaboxWindow).nabox.createSession();
+            console.log(naboxInfo)
+            const address = naboxInfo[0];
+            return new Promise((resolve, reject) => {
+                BigNumber.config({ DECIMAL_PLACES: 8 });
+
+                axios
+                    .post(
+                        ip1 + "api/accountledger/balance/" + address,
+                        {
+                            assetChainId: 1,
+                            assetId: 1,
+                        },
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        }
+                    )
+                    .then(function (response) {
+                        var data = response.data;
+                        let amountBal = data.data.available;
+                       /* var displayBal = new BigNumber(amountBal)
+                            .dividedBy(Math.pow(10, 8))
+                            .toString();*/
+                        setBalanceNuls(amountBal)
+
+                    })
+                    .catch(function (error) {});
+
+            });
+        }
+        getTokenBalance()
+    },[])
+
+    async function max(){
+
+        setValueIn(new BigNumber(balanceNuls).dividedBy(Math.pow(10, 8))
+            .toString())
+
+    }
+
 
     return(
         <div style={{
@@ -56,20 +119,24 @@ export const ModalInvest = ({display, displayToggle, project}) => {
                     </div>
                     <div style={{display:"flex", justifyContent:"space-between", marginTop:"10px"}}>
                         <div>Balance:</div>
-                        <div>342 NULS</div>
+                        <div>{new BigNumber(balanceNuls)
+                            .dividedBy(Math.pow(10, 8))
+                            .toString() + "NULS"}</div>
                     </div>
                     <div style={{display:"flex", backgroundColor:"white", borderRadius:"8px", marginTop:"6px", padding:"0px 0px"}}>
                         <div style={{width:"calc(100% - 50px)",  borderRadius:"8px"}}>
-                            <input style={{padding:"10px",
+                            <input id="investAmount" style={{padding:"10px",
                                 border:"0px",
                                 outline:"none",
                                 width:"100%",
                                 borderRadius:"4px",
                                 fontSize:"16px"
-                            }} type="number" placeholder="Ex: 100 NULS" min="1"/>
+                            }} type="number" value={valueIn} placeholder="Ex: 100 NULS" min="1"/>
                         </div>
                         <div style={{width:"50px"}}>
-                            <button style={{padding:"10px", border:"0px", fontWeight:"bold", cursor:"pointer", backgroundColor:"transparent"}}>Max</button>
+                            <button
+                                onClick={() => max()}
+                                style={{padding:"10px", border:"0px", fontWeight:"bold", cursor:"pointer", backgroundColor:"transparent"}}>Max</button>
                         </div>
                     </div>
                     <div style={{padding:"10px 0px"}}>
